93567702e498301784b6e01c62f7424f8b56dbc0
CYY <837023110@qq.com>
2018-03-27 23:37:05 +0800

    轮播2

---
+++ src/components/Swiper.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="swiper">
+      <swiper :options="swiperOption"  ref="mySwiper">  
+            <!-- 这部分放你要渲染的那些内容 -->  
+            <!-- <swiper-slide v-for="item in items">
+              <img :src="items">  
+            </swiper-slide> -->  
+          <swiper-slide v-for="(item,index) in items" :key="index">
+            <img :src="item">
+          </swiper-slide>
+
+
+            <!-- 这是轮播的小圆点 -->  
+          <div class="swiper-pagination" slot="pagination"></div>  
+      </swiper>  
+  </div>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+import a from "./../assets/1.jpg";
+export default {
+  name: "Swiper",
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {  
+    return {
+      items: [
+        a, "./assets/2.jpg", "./assets/3.jpg", "./assets/4.jpg"
+      ],
+      swiperOption: {
+        //是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
+        notNextTick: true,
+        pagination: ".swiper-pagination",
+        slidesPerView: "auto",
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 30,
+        onSlideChangeEnd: swiper => {
+          //这个位置放swiper的回调方法
+          this.page = swiper.realIndex + 1;
+          this.index = swiper.realIndex;
+        }
+      }
+    };
+  },
+  //定义这个sweiper对象
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
+  mounted() {
+    //这边就可以使用swiper这个对象去使用swiper官网中的那些方法
+    this.swiper.slideTo(0, 0, false);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('../assets/css/swiper.min.css');
+
+</style>
